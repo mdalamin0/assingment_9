365f260f2db613552b1f48594fd7cba4f20549d4
md al-amin <muhammadalamin809@gmail.com>
2023-04-09 23:54:58 +0800

    project setup and all dependencies install done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <h3 className="text-xl text-center font-semibold py-4">Welcome to my project</h3>
+    </div>
+  )
+}
+
+export default App
